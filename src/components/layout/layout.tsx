68d75e9e93994a4de1c9282297ef21c1c5bce76c
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2025-05-23 10:49:17 -0300

    ajustes e criação de menu personalizado na visualização mobile

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,16 @@
 import "./layout.css"
 import { Outlet } from "react-router"
+import { useState } from "react"
 
 export function Layout(){
+   const [show, setShow] = useState(false)
+
    return (
       <div>
         <header>
            <h1>CURIOSIDADES DE TECNOLOGIA</h1>
            <p>Tudo aquilo que você sempre quis saber sobre o mundo Tech, em um único lugar</p>
+
            <nav>
                <a href="/">Home</a>
                <a href="/noticias">Notícias</a>
@@ -14,6 +18,16 @@ export function Layout(){
                <a href="/video">Video</a>
                <a href="/fale-conosco">Fale Conosco</a>
            </nav>
+
+           <button className="menu" onClick={() => setShow(!show)}><img src="src/assets/menu.svg" alt="ícone de menu"/></button>
+
+           {show && <nav className="mobileMenu">
+               <a href="/">Home</a>
+               <a href="/noticias">Notícias</a>
+               <a href="/curiosidades">Curiosidades</a>
+               <a href="/video">Video</a>
+               <a href="/fale-conosco">Fale Conosco</a>
+           </nav>}         
         </header>
 
         <Outlet />
